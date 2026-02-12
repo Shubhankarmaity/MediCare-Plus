2ca6253ddc9ae11d9268867e74003e6ee58823a2
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-02-13 01:02:28 +0530

    Updated Backend

--- server/routes/debug.js
+++ server/routes/debug.js
@@ -10,7 +10,9 @@ router.get('/email', async (req, res) => {
 
         // define transporter
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // use SSL
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
